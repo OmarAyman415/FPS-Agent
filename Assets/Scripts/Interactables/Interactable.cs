f289c9fbaadbedbcbefddb6553783c4f71e1a67b
OmarAyman415 <o.ayman415@gmail.com>
2022-05-05 06:24:29 +0200

    added controlled door

--- Assets/Scripts/Interactables/Interactable.cs
+++ Assets/Scripts/Interactables/Interactable.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    // add or remove an interactionEvent component to this gameobject 
+    public bool useEvents;
+
     //Message displayed tp the player when looking at an interactable
+    [SerializeField]
     public string promptMessage = "This default message";
 
+    public virtual string OnLook(){
+        return promptMessage;
+    }
     //this function will be called from our player
     public void BaseInteract()
     {
+        if(useEvents)
+            GetComponent<InteractionEvent>().onInteract.Invoke();
         Interact();
     }
     protected virtual void Interact() { }
---
+++ Assets/Scripts/Interactables/InteractionEvent.cs
@@ -0,0 +1,8 @@
+
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InteractionEvent : MonoBehaviour
+{
+    public UnityEvent onInteract;
+}
